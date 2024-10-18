5ea875c11bc2ef9e3cc36919e35480f008b1b1b3
Piyal Guho Dipto <diptoguho4@gmail.com>
2024-10-18 15:35:23 +0600

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,24 @@
+import AmountSummary from "./AmountSummary";
+import Expense from "./expenseSummary.jsx/Expense";
+import Income from "./expenseSummary.jsx/Income";
+import InputForm from "./InputForm";
+
+export default function Main() {
+  return (
+    <>
+      <main className="relative mx-auto mt-10 w-full max-w-7xl">
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <InputForm />
+
+          <div className="lg:col-span-2">
+            <AmountSummary />
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-8">
+              <Income />
+              <Expense />
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
