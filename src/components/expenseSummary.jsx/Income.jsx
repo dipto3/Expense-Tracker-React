dc762e03ecaa6406c8b458835721813c9f70af49
Piyal Guho Dipto <diptoguho4@gmail.com>
2024-10-19 02:30:40 +0600

    sgv component

--- src/components/expenseSummary.jsx/Income.jsx
+++ src/components/expenseSummary.jsx/Income.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import Button from "../Button";
 import AmountFilterSvg from "../svg/AmountFilterSvg";
+import DeleteSvg from "../svg/DeleteSvg";
+import EditSvg from "../svg/EditSvg";
 import IncomeSvg from "../svg/IncomeSvg";
 import SettingSvg from "../svg/SettingSvg";
 
 export default function Income() {
+  const [activeFilter, setActiveFilter] = useState(false);
+  const [activeCategoryFilter, setActiveCategoryFilter] = useState(false);
+
+  function handleClickAmountFilter() {
+    setActiveFilter(!activeFilter);
+    // console.log(activeFilter);
+  }
+  function handleClickCategoryFilter() {
+    setActiveCategoryFilter(!activeCategoryFilter);
+    console.log(activeCategoryFilter);
+  }
   return (
     <>
       <div className="border rounded-md relative">
@@ -21,104 +36,104 @@ export default function Income() {
           <div>
             <div className="relative inline-block text-left">
               <div>
-                <button
-                  type="button"
+                <Button
+                  onSmash={handleClickAmountFilter}
+                  type="submit"
                   className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                  id="menu-button"
-                  aria-expanded="true"
-                  aria-haspopup="true"
                 >
                   <AmountFilterSvg />
-                </button>
+                </Button>
               </div>
 
-              <div
-                className="absolute z-10 mt-2 left-5 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="menu-button"
-                tabIndex="-1"
-              >
-                <div className="py-1" role="none">
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-all"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="menu-item-0"
-                  >
-                    Low to High
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-all"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="menu-item-0"
-                  >
-                    High to Low
-                  </a>
+              {activeFilter && (
+                <div
+                  className="absolute z-10 mt-2 left-5 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="menu-button"
+                  tabIndex="-1"
+                >
+                  <div className="py-1" role="none">
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-all"
+                      role="menuitem"
+                      tabIndex="-1"
+                      id="menu-item-0"
+                    >
+                      Low to High
+                    </a>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-all"
+                      role="menuitem"
+                      tabIndex="-1"
+                      id="menu-item-0"
+                    >
+                      High to Low
+                    </a>
+                  </div>
                 </div>
-              </div>
+              )}
             </div>
 
             <div className="relative inline-block text-left">
               <div>
-                <button
-                  type="button"
+                <Button
+                  onSmash={handleClickCategoryFilter}
+                  type="submit"
                   className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                  id="filter-button"
-                  aria-expanded="true"
-                  aria-haspopup="true"
                 >
                   <SettingSvg />
-                </button>
+                </Button>
               </div>
 
-              <div
-                className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="filter-button"
-                tabIndex="-1"
-                id="filter-dropdown"
-              >
-                <div className="py-1" role="none">
-                  <label className="inline-flex items-center px-4 py-2 text-sm text-gray-700">
-                    <input
-                      type="checkbox"
-                      className="form-checkbox h-4 w-4 rounded-md text-gray-600"
-                      id="filter-option-1"
-                    />
-                    <span className="ml-2">Salary</span>
-                  </label>
-                  <label className="inline-flex items-center px-4 py-2 text-sm text-gray-700">
-                    <input
-                      type="checkbox"
-                      className="form-checkbox h-4 w-4 rounded-md text-gray-600"
-                      id="filter-option-2"
-                    />
-                    <span className="ml-2">Outsourcing</span>
-                  </label>
-                  <label className="inline-flex items-center px-4 py-2 text-sm text-gray-700">
-                    <input
-                      type="checkbox"
-                      className="form-checkbox h-4 w-4 rounded-md text-gray-600"
-                      id="filter-option-3"
-                    />
-                    <span className="ml-2">Bond</span>
-                  </label>
+              {activeCategoryFilter && (
+                <div
+                  className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="filter-button"
+                  tabIndex="-1"
+                  id="filter-dropdown"
+                >
+                  <div className="py-1" role="none">
+                    <label className="inline-flex items-center px-4 py-2 text-sm text-gray-700">
+                      <input
+                        type="checkbox"
+                        className="form-checkbox h-4 w-4 rounded-md text-gray-600"
+                        id="filter-option-1"
+                      />
+                      <span className="ml-2">Salary</span>
+                    </label>
+                    <label className="inline-flex items-center px-4 py-2 text-sm text-gray-700">
+                      <input
+                        type="checkbox"
+                        className="form-checkbox h-4 w-4 rounded-md text-gray-600"
+                        id="filter-option-2"
+                      />
+                      <span className="ml-2">Outsourcing</span>
+                    </label>
+                    <label className="inline-flex items-center px-4 py-2 text-sm text-gray-700">
+                      <input
+                        type="checkbox"
+                        className="form-checkbox h-4 w-4 rounded-md text-gray-600"
+                        id="filter-option-3"
+                      />
+                      <span className="ml-2">Bond</span>
+                    </label>
 
-                  <label className="inline-flex items-center px-4 py-2 text-sm text-gray-700">
-                    <input
-                      type="checkbox"
-                      className="form-checkbox h-4 w-4 rounded-md text-gray-600"
-                      id="filter-option-3"
-                    />
-                    <span className="ml-2">Dividend</span>
-                  </label>
+                    <label className="inline-flex items-center px-4 py-2 text-sm text-gray-700">
+                      <input
+                        type="checkbox"
+                        className="form-checkbox h-4 w-4 rounded-md text-gray-600"
+                        id="filter-option-3"
+                      />
+                      <span className="ml-2">Dividend</span>
+                    </label>
+                  </div>
                 </div>
-              </div>
+              )}
             </div>
           </div>
         </div>
@@ -142,21 +157,7 @@ export default function Income() {
                   role="button"
                   title="Edit Button"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
-                    <path d="M13.5 6.5l4 4" />
-                  </svg>
+                  <EditSvg />
                 </button>
 
                 <button
@@ -164,91 +165,13 @@ export default function Income() {
                   role="button"
                   title="Delete"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M4 7l16 0" />
-                    <path d="M10 11l0 6" />
-                    <path d="M14 11l0 6" />
-                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                  </svg>
+                  <DeleteSvg />
                 </button>
               </div>
             </div>
           </div>
 
-          <div className="flex justify-between items-center py-2 relative group cursor-pointer">
-            <div>
-              <h3 className="text-base font-medium leading-7 text-gray-600">
-                Salary
-              </h3>
-              <p className="text-xs text-gray-600">15 January 2024</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <p className="text-base font-semibold text-gray-600 transition-all group-hover:-translate-x-14">
-                BDT 10000
-              </p>
-
-              <div className="translate-x-5 group-hover:translate-x-0 opacity-0 group-hover:opacity-100 absolute right-0 top-1/2 -translate-y-1/2 transition-all">
-                <button
-                  className="hover:text-teal-600"
-                  role="button"
-                  title="Edit Button"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
-                    <path d="M13.5 6.5l4 4" />
-                  </svg>
-                </button>
-
-                <button
-                  className="hover:text-red-600"
-                  role="button"
-                  title="Delete"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M4 7l16 0" />
-                    <path d="M10 11l0 6" />
-                    <path d="M14 11l0 6" />
-                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </div>
+          
         </div>
       </div>
     </>
